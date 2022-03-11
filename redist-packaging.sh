af02de5f085a309083cdb20b8bd47d81db64636d
Jean Guillaume <jgi@jgwill.com>
2022-03-11 12:24:02 -0500

    redist

---
+++ redist-packaging.sh
@@ -0,0 +1,5 @@
+sed -i 's/burtonator/GuillaumeIsabelleX/' package.json   
+sed -i 's/polar-shared/gix-polar-shared/'
+sed -i 's/polar-shared/gix-polar-shared/' package.json    
+npm install
+npm publish
